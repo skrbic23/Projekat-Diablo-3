b9c9ace6cfca933942627b8fa64d9aa14f28ed56
skrbic23 <63725782+skrbic23@users.noreply.github.com>
2020-04-15 15:44:42 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+class Barbarian{
+private:
+    string opis;
+    string skilovi;
+    string pasivne;
+    string pol;
+    unsigned lvl;
+
+public:
+    Barbarian(){
+
+    };
+    Barbarian();
+    Barbarian();
+};
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
